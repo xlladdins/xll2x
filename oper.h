9e7a7b70570f26c68c1b8b8a12377e1b0da606c5
Keith A. Lewis <kal@kalx.net>
2023-10-16 18:02:12 -0400

    compiling

--- oper.h
+++ oper.h
@@ -1,32 +1,60 @@
-// oper.h - C++ wrapper for XLOPER12 struct
+// oper.h - C++ wrapper for XLOPER struct
 #pragma once
+#include <concepts>
 #include "xll.h"
 
 namespace xll {
 
-	struct OPER : public XLOPER12 {
+	// null terminated array length
+	template<class T>
+	constexpr size_t len(const T* p, size_t n = 0)
+	{
+		return *p ? len(p + 1, n + 1) : n;
+	}
 
+	template<class X>
+	struct oper_traits {
+	};
+
+	template<>
+	struct oper_traits<XLOPER> {
+		using xchar = CHAR;
+	};
+
+	template<>
+	struct oper_traits<XLOPER12> {
+		using xchar = XCHAR;
+	};
+
+	template<class X>
+		requires std::is_same_v<X, XLOPER> || std::is_same_v<X, XLOPER12>
+	struct XOPER : public X {
+		using X::xltype;
+		using X::val;
+
+		// Type without allocation bits.
 		constexpr DWORD type() const
 		{
 			return xltype & (~xlbitDLLFree | ~xlbitXLFree);
 		}
 
-		constexpr OPER()
+		// Default constructor.
+		constexpr XOPER()
 		{
 			xltype = xltypeNil;
 		}
 
 		// 64-bit double precison floating point
-		constexpr explicit OPER(double num) noexcept
+		constexpr explicit XOPER(double num) noexcept
 		{
 			xltype = xltypeNum;
 			val.num = num;
 		}
-		OPER& operator=(double num)
+		XOPER& operator=(double num)
 		{
-			free();
+			deallocate();
 
-			return *this = OPER(num);
+			return *this = XOPER(num);
 		}
 		constexpr bool operator==(double num) const
 		{
@@ -34,34 +62,34 @@ namespace xll {
 		}
 
 		// counted wide string
-		OPER(size_t n, XCHAR const* str)
+		XOPER(oper_traits<X>::xchar const* str, size_t n)
 		{
 			ensure(n <= std::numeric_limits<XCHAR>::max()/2);
 			xltype = xltypeStr;
 			val.str[0] = static_cast<XCHAR>(n);
-			str = new XCHAR[](1 + val.str[0]);
+			str = new oper_traits<X>::xchar[](1 + val.str[0]);
 			wcscpy_s(1 + val.str, n, str);
 		}
-		explicit OPER(XCHAR const* str)
-			: OPER(wcslen(str), str)
+		explicit XOPER(oper_traits<X>::xchar const* str)
+			: XOPER(str, len(str))
 		{ }
-		OPER& operator=(XCHAR const* str)
+		XOPER& operator=(oper_traits<X>::xchar const* str)
 		{
-			free();
+			deallocate();
 
-			return *this = OPER(str);
+			return *this = XOPER(str);
 		}
 		bool operator==(XCHAR const* str) const
 		{
 			size_t n = wcslen(str);
 
-			return n <= std::numeric_limits<XCHAR>::max()
-				&& val.str[0] == static_cast<XCHAR>(n)
+			return n <= std::numeric_limits<oper_traits<X>::xchar>::max()
+				&& val.str[0] == static_cast<oper_traits<X>::xchar>(n)
 				&& 0 == wcsncmp(val.str + 1, str, n);
 		}
 		
 		// Boolean
-		constexpr explicit OPER(bool xbool) noexcept
+		constexpr explicit XOPER(bool xbool) noexcept
 		{
 			xltype = xltypeBool;
 			val.xbool = xbool;
@@ -71,13 +99,14 @@ namespace xll {
 			return type() == xltypeBool && !!val.xbool == xbool;
 		}
 
-		~OPER()
+		~XOPER()
 		{
-			free();
+			deallocate();
 		}
 	private:
-		void free()
+		void deallocate()
 		{
+			// No xlbitDLLFree or xlbitXLFree bits set.
 			if (xltype == xltypeStr) {
 				delete[] val.str;
 			}
@@ -85,5 +114,8 @@ namespace xll {
 			xltype = xltypeNil;
 		}
 	};
+
+	using OPER = XOPER<XLOPER12>;
+	using OPER4 = XOPER<XLOPER>;
 
 } // namespace xll
